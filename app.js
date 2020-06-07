4745c28626e7b678fb6e348dc5b9c1530e9ce18b
Kamal <itpothitech@gmail.com>
2020-06-07 17:54:44 +0530

    set only update

--- app.js
+++ app.js
@@ -50,27 +50,3 @@ app.post("/upload-user-profile", (req, res) => {
     res.send(`You have successfully uploaded file: ${req.file.path}`);
   });
 });
-
-app.post("/update-user-profile", (req, res) => {
-  let upload = multer({
-    storage: storage,
-    fileFilter: helpers.docFilter,
-  }).single("excel_update");
-
-  upload(req, res, function (err) {
-    // req.file contains information of uploaded file
-
-    if (req.fileValidationError) {
-      return res.send(req.fileValidationError);
-    } else if (!req.file) {
-      return res.send("Please select a excel document to update");
-    } else if (err instanceof multer.MulterError) {
-      return res.send(err);
-    } else if (err) {
-      return res.send(err);
-    }
-
-    save.saveProfiles(req.file);
-    res.send(`You have successfully uploaded file: ${req.file.path}`);
-  });
-});
--- save.js
+++ save.js
@@ -1,15 +1,11 @@
 const uploadProfile = require("./scrape-profiles/lib/upload-profiles");
 
 const saveProfiles = async (inputs) => {
-  //   console.log("inputs =>", inputs);
-  let operation = "";
+  let operation = "update";
   const fileName = inputs.path;
-  if (inputs.fieldname == "excel_upload") {
-    operation = "create";
-  } else if (inputs.fieldname == "excel_update") {
-    operation = "update";
-  }
+
   const options = { file: fileName, ops: operation };
+  console.log(options);
   result = await uploadProfile(options);
   console.log(result);
   return result;
